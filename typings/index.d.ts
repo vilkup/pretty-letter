d2d386999b497e98e4868bacd6d6575d8ed216d9
Vilen Kupriienko <pride2808@gmail.com>
2020-02-25 19:35:36 +0200

    Typings were added

---
+++ typings/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Returns a letter depending on the quantity
+ *
+ * Imagine you are only working with digits :)
+ * Use letters[0] for 1, letters[1] for 2..4, letters[2] for 5..9 and 0
+ *
+ * @function
+ * @param {number} number     – quantity to depend on
+ * @param {string[]} letters  - array of letters to choose from
+ *
+ * @return {string} letter
+ * */
+export default function prettyLetter(number: number, letters: string[]): string
