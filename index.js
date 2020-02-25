64509c60be8387e452d36e0674141cfe30d001eb
Vilen Kupriienko <pride2808@gmail.com>
2020-02-25 18:52:29 +0200

    v.1.0.0

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Returns a letter depending on the quantity
+ *
+ * Imagine you are only working with digits :)
+ * Use letters[0] for 1, letters[1] for 2..4, letters[2] for 5..9 and 0
+ *
+ * @function
+ * @param {number} number     – quantity to depend on
+ * @param {string[]} letters  - array of letters to choose from
+ *
+ * @return {string} letter
+ * */
+const prettyLetter = (number, letters) => {
+  number = Math.abs(number).toString()
+
+  const numberIsFractional = number.includes('.')
+  const [integerPart] = number.split('.')
+
+  const digitsCount = integerPart.length
+
+  const firstDigit = +integerPart[0]
+  const lastDigit = +integerPart[digitsCount - 1]
+
+  if (digitsCount === 1) {
+    if (firstDigit === 1) {
+      return letters[0]
+    }
+    if (firstDigit > 1 && firstDigit < 5 || numberIsFractional && +integerPart === 0) {
+      return letters[1]
+    }
+    if (firstDigit === 0 || firstDigit >= 5 || numberIsFractional) {
+      return letters[2]
+    }
+  }
+  else {
+    if (lastDigit === 1 && firstDigit !== 1) {
+      return letters[0]
+    }
+    if (lastDigit > 1 && lastDigit < 5 || numberIsFractional && +integerPart === 0) {
+      return letters[1]
+    }
+    if (lastDigit === 0 || lastDigit >= 5 || firstDigit === 1 || numberIsFractional) {
+      return letters[2]
+    }
+  }
+}
+
+module.exports = prettyLetter
---
+++ test/index.test.js
@@ -0,0 +1,43 @@
+const pl = require('..')
+
+const letters = ['й', 'я', 'ев']
+
+test('"21 комментарий" resolves the right way', () => {
+  const letter = pl(21, letters)
+  return expect(letter).toEqual('й')
+})
+
+test('"0.12 комментария" resolves the right way', () => {
+  const letter = pl(0.12, letters)
+  return expect(letter).toEqual('я')
+})
+
+test('"-15.21 комментариев" resolves the right way', () => {
+  const letter = pl(-15.21, letters)
+  return expect(letter).toEqual('ев')
+})
+
+test('"11.0 комментариев" resolves the right way', () => {
+  const letter = pl(11.0, letters)
+  return expect(letter).toEqual('ев')
+})
+
+test('"-52 комментария" resolves the right way', () => {
+  const letter = pl(-52, letters)
+  return expect(letter).toEqual('я')
+})
+
+test('"-6 комментариев" resolves the right way', () => {
+  const letter = pl(-6, letters)
+  return expect(letter).toEqual('ев')
+})
+
+test('"1 комментарий" resolves the right way', () => {
+  const letter = pl(1, letters)
+  return expect(letter).toEqual('й')
+})
+
+test('"55 комментариев" resolves the right way', () => {
+  const letter = pl(55, letters)
+  return expect(letter).toEqual('ев')
+})
